c1523c0ff5b9daea5c414aa02be0e151a638bbb8
Guillermo <GuillermoChaveste@ciencias.unam.mx>
2017-08-17 20:15:23 -0500

    Creacion un Hola Mundo en c

---
+++ HolaMundo.c
@@ -0,0 +1,11 @@
+/* Programa que impreme en consola un Hola Mundo */
+#include <stdio.h>
+
+/* Función principal */
+int main()
+{
+	/* Impresión por pantalla y salida del programa*/
+    printf("Hola Mundo!\n");
+    return 0;
+}
+
