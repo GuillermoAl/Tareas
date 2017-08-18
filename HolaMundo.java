c531a47309291ca6e9c8f2a9f641cada6526f8d4
Guillermo <GuillermoChaveste@ciencias.unam.mx>
2017-08-17 20:13:44 -0500

    Creacion un Hola Mundo en java

---
+++ HolaMundo.java
@@ -0,0 +1,15 @@
+/** 
+    * Programa que imprime Hola Mundo  
+	* @author Guillermo
+	*/
+
+public class HolaMundo{
+	/** Método main donde se monstrará el mensaje de salida
+		* @params args Arreglo de String's
+        * @return Sin valor de retorno 
+   		*/
+   		
+     public static void main(String []args){
+        System.out.println("Hola Mundo!");
+     }
+}
