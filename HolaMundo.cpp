84139c68463ab33814f27252f42f1b4aca037c3a
Guillermo <GuillermoChaveste@ciencias.unam.mx>
2017-08-17 20:15:55 -0500

    Creacion un Hola Mundo en c++

---
+++ HolaMundo.cpp
@@ -0,0 +1,13 @@
+/* Programa que impreme en consola un Hola Mundo */
+#include <iostream>
+
+using namespace std;
+
+/* Función principal */
+int main()
+{
+	/* Impresión por pantalla y salida del programa*/
+	cout << "Hola Mundo" << endl; 
+	return 0;
+
+}	
